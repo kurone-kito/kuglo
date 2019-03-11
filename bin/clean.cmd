a67ae9f8ee6edc04b84de6589e5ae784e170b0c3
kurone-kito <krone@kit.black>
2019-03-11 20:57:57 +0900

    Improved the fully clean-up script

--- bin/clean.cmd
+++ bin/clean.cmd
@@ -5,5 +5,8 @@ REM # are broken, the clean up script needs to be workable,
 REM # so it is implemented by a Windows batch script.
 
 CD /d %~dp0\..
+RMDIR /S /Q coverage >NUL 2>&1
 RMDIR /S /Q dist >NUL 2>&1
+RMDIR /S /Q logs >NUL 2>&1
 RMDIR /S /Q node_modules >NUL 2>&1
+RMDIR /S /Q storybook-static >NUL 2>&1
