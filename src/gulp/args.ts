64bd9ea5ff50aac7ae709df313797cb932ffa9b4
kurone-kito <krone@kit.black>
2019-03-25 06:35:11 +0900

    Moved the definition of internal args for Gulp

--- src/gulp/args.ts
+++ src/gulp/args.ts
@@ -1,33 +1,13 @@
 import minimist from 'minimist';
 import { options as binaryBuilderOptions } from './buildElectronAsync';
+import { options as cleanOptions } from './cleanAsync';
 import { options as contentBuilderOptions } from './contentBuilder';
-
-/** Default values of options for building redistributable package. */
-export const packageBuildOptions = Object.freeze({
-  /** For Linux. */
-  linux: true,
-  /** For macOS. */
-  macos: false,
-  /** For Windows. */
-  windows: true
-});
-
-/** Default values of options for cleaning. */
-export const cleanOptions = Object.freeze({
-  /** Remove the logs. */
-  logs: false,
-  /** Remove the built data. */
-  dist: false,
-  /** Remove the built data of Storybook. */
-  storybook: false
-});
 
 /** Combined default values. */
 const defaultArgs = {
   ...binaryBuilderOptions,
   ...contentBuilderOptions,
-  ...cleanOptions,
-  ...packageBuildOptions
+  ...cleanOptions
 };
 
 /** Type of combined default values. */
--- src/gulp/cleanAsync.ts
+++ src/gulp/cleanAsync.ts
@@ -1,8 +1,18 @@
 import rmfr from 'rmfr';
-import getArgs, { cleanOptions } from './args';
+import getArgs from './args';
+
+/** Default values of options for cleaning. */
+export const options = Object.freeze({
+  /** Remove the logs. */
+  logs: false,
+  /** Remove the built data. */
+  dist: false,
+  /** Remove the built data of Storybook. */
+  storybook: false
+});
 
 /** CLI Options definition. */
-type OptionsType = typeof cleanOptions;
+type OptionsType = typeof options;
 
 /** Keys definition of CLI options definition. */
 type Keys = keyof OptionsType;
@@ -20,16 +30,16 @@ const forceRemoveAsync = (targets: string[]) =>
   );
 
 /** Keys list of CLI options definition. */
-const keys = Object.keys(cleanOptions) as Keys[];
+const keys = Object.keys(options) as Keys[];
 
 /**
  * Remove specific files.
- * @param options Options.
+ * @param o Options.
  *
  * It's apply prior to CLI options.
  */
-const cleanAsync = async (options: Partial<OptionsType> = cleanOptions) => {
-  const args = getArgs(options);
+const cleanAsync = async (o: Partial<OptionsType> = options) => {
+  const args = getArgs(o);
   const all = keys.reduce((p, key) => p && !args[key], true);
   const remove = (key: Keys, targets: string[]) =>
     all || args[key] ? forceRemoveAsync(targets) : undefined;
