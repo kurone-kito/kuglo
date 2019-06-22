4f6177106a93df93192c208db20e4f30efd00a5c
kurone-kito <krone@kit.black>
2019-06-22 22:22:22 +0900

    Simplefied the cleanup scripts

--- bin/clean.cmd
+++ bin/clean.cmd
@@ -5,8 +5,4 @@ REM # are broken, the clean up script needs to be workable,
 REM # so it is implemented by a Windows batch script.
 
 CD /d %~dp0\..
-RMDIR /S /Q coverage >NUL 2>&1
-RMDIR /S /Q dist >NUL 2>&1
-RMDIR /S /Q logs >NUL 2>&1
-RMDIR /S /Q node_modules >NUL 2>&1
-RMDIR /S /Q storybook-static >NUL 2>&1
+git clean -Xdf
